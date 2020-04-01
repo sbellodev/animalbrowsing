5c172682cd56c8fb550976240d1e36173589aace
debello <42223093+debello@users.noreply.github.com>
2020-04-01 03:44:39 +0200

    Add example of table

--- src/content/Table.js
+++ src/content/Table.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import styled from 'styled-components'
+import bug1 from '../img/bitterling.png'
 
 const Table = () => {
     return (
         <>
             <BugsTable>
                 <thead>
-                    <th>Insecto</th>
-                    <th>Precio</th>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Time</th>
+                    <th>Season</th>
+                    <th>Location <br/> Size</th>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Bichopalo</td>
-                        <td>Fanni</td>
+                        <td><img width="50px" alt="sudo" src={bug1} /></td>
+                        <td>Common Butterfly</td>
+                        <td>1000</td>
+                        <td>4 AM <br/>7 PM</td>
+                        <td>Sep-Jun (Northern) /<br/> Mar-Dec (Southern)</td>
+                        <td>River <br/>Big</td>
                     </tr>
                     <tr>
-                        <td>1.000</td>
-                        <td>1 euro</td>
+                        <td><img width="50px" alt="sudo" src={bug1} /></td>
+                        <td>Common Butterfly</td>
+                        <td>1000</td>
+                        <td>4 AM <br/>7 PM</td>
+                        <td>Sep-Jun (Northern) /<br/> Mar-Dec (Southern)</td>
+                        <td>River <br/>Big</td>
+                    </tr>
+                    <tr>
+                        <td><img width="50px" alt="sudo" src={bug1} /></td>
+                        <td>Common Butterfly</td>
+                        <td>1000</td>
+                        <td>4 AM <br/>7 PM</td>
+                        <td>Sep-Jun (Northern) /<br/> Mar-Dec (Southern)</td>
+                        <td>River <br/>Big</td>
+                    </tr>
+                    <tr>
+                        <td><img width="50px" alt="sudo" src={bug1} /></td>
+                        <td>Common Butterfly</td>
+                        <td>1000</td>
+                        <td>4 AM <br/>7 PM</td>
+                        <td>Sep-Jun (Northern) /<br/> Mar-Dec (Southern)</td>
+                        <td>River <br/>Big</td>
                     </tr>
                 </tbody>
             </BugsTable>
@@ -25,9 +54,11 @@ const Table = () => {
 }
 const BugsTable = styled.table`
     background-color: skyblue;
-    margin: 50px;
     text-align: center;
     border-collapse: collapse;
+    td {
+        height: 60px;
+    }
 `
 
 export { Table }
