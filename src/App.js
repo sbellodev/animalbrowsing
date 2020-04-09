1c3e992e4076239c6820bae1fc70d0206deea333
debello <42223093+debello@users.noreply.github.com>
2020-04-09 18:32:41 +0200

    Add NavBar and Description sections. Add half circle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
+import styled from 'styled-components'
 import { Table } from './content/Table'
 
 const App = () => {
   return (
-    <div>
-      <div>Navegador</div>
-      <div>Titulo seccion</div>
+    <AppContainer>
+      <NavBar>Navegador</NavBar>
+      <Section><p>Title Description</p>
+      <Ellipsis></Ellipsis></Section>
+      
       <Table />
-    </div>
+    </AppContainer>
   );
 }
+const AppContainer = styled.div`
+  p {
+    margin: 0
+  }
+`
+const NavBar = styled.div`
+  max-width: 800px;
+  height: 60px;
+  background-color: tomato;
 
+`
+const Section = styled.div`
+  max-width: 800px;
+  height: 190px;
+  background-color: orangered;
+  position: relative;
+`
+const Ellipsis = styled.div`
+  width: 100%;
+  height: 80px;
+  background-color: black;
+  border-top-left-radius: 100%;
+  border-top-right-radius: 100%;
+  position:absolute;                  
+  bottom:0;                          
+  left:0;
+  background-color: skyblue;
+`
 export default App;
