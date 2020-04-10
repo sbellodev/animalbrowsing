5652ab1ba2c0b3e42356102cf06f07d339ccb682
debello <42223093+debello@users.noreply.github.com>
2020-04-10 22:53:40 +0200

    Show tables, add font, colors

--- src/App.js
+++ src/App.js
@@ -1,47 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 import { Table } from './content/Table'
-//import imgPrice from '../img/icons/price1.jpg'
+import comicsans1 from './font/comic.TTF'
+import comicsans2 from './font/comici.ttf'
+import comicsans3 from './font/comicmsgras.ttf'
+import afont from './font/afont.otf'
 
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Comic1';
+    src: url(${comicsans1}) format('truetype');
+  }
+  @font-face {
+    font-family: 'Comic2';
+    src: url(${comicsans2}) format('truetype');
+  }
+  @font-face {
+    font-family: 'Comic3';
+    src: url(${comicsans3}) format('truetype');
+  }
+  @font-face {
+    font-family: 'afont';
+    src: url(${afont}) format('opentype');
+  }
+
+  body {
+    background-color: #F5F2E3;
+
+    p, h1, h2, h3, h4, h5, h6 {
+      margin: 0;
+    }
+    a, a:hover, a:visited {
+      text-decoration: none;
+      color: inherit;
+    }
+    button {
+      border: 0;
+    }
+  }
+ ` 
+  //import imgPrice from '../img/icons/price1.jpg'
+
+const primaryColor = {
+  NavBackground: "#FFFFFF",
+  BackgroundPrimary: "#F5F2E3", 
+  LetterPrimary: "FFFFFF", 
+  Orange: "#FFFFFF", 
+  TableBackground: "#FFFFFF", 
+  TableSecondary: "#FFFFFF", 
+  ButtonPrimary: "#FFFFFF", 
+  Skyblue: "#A0D0E7", 
+  Orangered: "#FFB347"
+}
 const imageURL = {
   Price: "/img/icons/price1.jpg", 
   ABC: "/img/icons/abc.png", 
   Search: "/img/icons/search1.jpg", 
-  Reset: "/img/icons/reset1.png"
+  Reset: "/img/icons/reset1.png", 
+  Return: "/img/icons/return1.png"
 }
+
+
 const App = () => {
+  const [index, setIndex] = useState("Bugs");
+
   return (
     <AppContainer>
+      <GlobalStyles />
       <NavBar>
-        <HomeIndex>Home</HomeIndex>
-        <BugIndex>Insects</BugIndex>
-        <FishIndex>Fishes</FishIndex>
+        <HomeIndex onClick={() => {setIndex("Home")}}>Home</HomeIndex>
+        <BugIndex onClick={() => {setIndex("Bugs")}}>Bugs</BugIndex>
+        <FishIndex onClick={() => {setIndex("Fish")}}>Fishes</FishIndex>
       </NavBar>
       <Section>
-        <h2>Insects</h2>
-        <Ellipsis>
-          <SearchButton><img src={imageURL.Search} width={"30px"} alt="Search" /></SearchButton>
-          <ABCButton><img src={imageURL.ABC} width={"100%"} alt="ABC" /></ABCButton>
-          <PriceButton><img src={imageURL.Price} width={"100%"} alt="price" /></PriceButton>
-          <ResetButton><img src={imageURL.Reset} width={"100%"} alt="Reset" /></ResetButton>
-        </Ellipsis>
+        <Description>
+          <h2>{index}</h2>
+          <h5>If needed, you can use the search option or order by price / alphabetically.</h5>
+          <h5>¡Good luck! And take care</h5>
+        </Description>
+
       </Section>
+      <Ellipsis>
+          <SearchButton onClick={() => console.log("Buttone Search pulsao")}><SearchImage src={imageURL.Search}  alt="Search" /></SearchButton>
+          <ABCButton onClick={() => console.log("Buttone ABC pulsao")}><ABCImage src={imageURL.ABC}  alt="ABC" /></ABCButton>
+          <PriceButton onClick={() => console.log("Buttone Price pulsao")}><PriceImage src={imageURL.Price}  alt="price" /></PriceButton>
+          <ResetButton onClick={() => console.log("Buttone Reset pulsao")}><ResetImage src={imageURL.Reset}  alt="Reset" /></ResetButton>
+        </Ellipsis>
       
-      <Table />
+      <Table actualIndex={index} />
+      <ReturnImage src={imageURL.Return} width={"40px"} alt="Back to top" />
     </AppContainer>
   );
 }
 const AppContainer = styled.div`
-  p, h1, h2, h3, h4, h5 {
-    margin: 0;
-  }
-  button {
-    border: 0;
-  }
+  
 `
 const NavBar = styled.div`
+  font-family: "afont";
   max-width: 800px;
   height: 60px;
   background-color: tomato;
@@ -56,30 +113,31 @@ const NavBar = styled.div`
   }
 `
 const HomeIndex = styled.p`
-  color: #fbeded;
+  color: #F5F2E3;
   font-weight: bold;
   text-align: center;
 `
 const BugIndex = styled.p`
-  color: #fbeded;
+  color: #F5F2E3;
   font-weight: bold;
   text-align: center;
 `
 const FishIndex = styled.p`
-  color: #fbeded;
+  color: #F5F2E3;
   font-weight: bold;
   text-align: center;
 `
 
 const Section = styled.div`
   max-width: 800px;
-  height: 190px;
-  background-color: orangered;
+  height: 155px;
+  background-color: #F5F2E3;
   position: relative;
-  h2 {
+`
+const Description = styled.div`
+    font-family: afont;
     text-align: center;
-    padding-top: 10%;
-  }
+    padding: 3% 3% 0 3%; 
 `
 const Ellipsis = styled.div`
   width: 100%;
@@ -87,10 +145,7 @@ const Ellipsis = styled.div`
   background-color: black;
   border-top-left-radius: 100%;
   border-top-right-radius: 100%;
-  position:absolute;                  
-  bottom:0;                          
-  left:0;
-  background-color: skyblue;
+  background-color: #A0D0E7;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
@@ -121,6 +176,23 @@ const ResetButton = styled.button`
   height: 30px;
   margin-top: 45px;
   border-radius: 50%;
+`
+const PriceImage = styled.img`
+  width: 100%;
+`
+const ABCImage = styled.img`
+  width: 100%;
+`
+const SearchImage = styled.img`
+  width: 30px;
+`
+const ResetImage = styled.img`
+  width: 100%;
+`
+const ReturnImage = styled.img`
+  width: 40px;
+  display: block;
+  margin: auto;
 `
 
 export default App;
--- src/content/Table.js
+++ src/content/Table.js
@@ -2,9 +2,6 @@ import React from 'react';
 import styled from 'styled-components'
 import bugInfo from '../data/bug.json'
 import fishInfo from '../data/fish.json'
-//import testdata from '../data/test.json'
-//import image from '../img/bitterling.png'
-
 
 // const BugDesktopTable = () => {
 //    const row = bugInfo.map(value =>
@@ -21,17 +18,29 @@ import fishInfo from '../data/fish.json'
 //         {row}
 //     )
 // }
-const BugMiniTable = () => {
+
+const BugMobileTable = () => {
     const row = bugInfo.map(value =>
          <tr key={value.Number}>
-            <td><img src={"../img/bug/" +value.Image} alt="sudando" /></td>
-            <td>{value.Name}<br/>{value.Price}</td>
-            <td> {value.Time} <br/> {value.Location}</td>
+            <td><img src={"../img/bug/" + value.Image} alt={value.Name} /></td>
+            <td>{value.Name} <br/> {value.Price}</td>
+            <td>{value.Time} <br/> {value.Location}</td>
             <td dangerouslySetInnerHTML={{ __html: value.Season}}></td>
          </tr>
      )
      return row
- }
+}
+const FishMobileTable = () => {
+    const row = fishInfo.map(value =>
+         <tr key={value.Number}>
+            <td><img src={"../img/fish/" + value.Image} alt={value.Name} /></td>
+            <td>{value.Name}<br/>{value.Price}</td>
+            <td>{value.Time}<br/>{value.Location}<br/>{value.Size}</td>
+            <td dangerouslySetInnerHTML={{ __html: value.Season}}></td>
+         </tr>
+     )
+     return row
+}
 // const FishDesktopTable = () => {
     
 //     const row = fishInfo.map(value =>
@@ -47,53 +56,87 @@ const BugMiniTable = () => {
 //      return row
 //  }
 
-const Table = () => {
+const Table = ({actualIndex}) => {
     // todo
     //if desktop return
     // code
 
     //if mobile return
-    return (
-        <BugTable>
-            <thead>
-                <tr>
-                    <th>Image</th>
-                    <th>Name <br/> Price</th>
-                    <th>Time <br/>Location</th>
-                    <th>Season <br/> (Hemi.)</th>
-                </tr>
-            </thead>
-            <tbody>
-                {BugMiniTable()}
-            </tbody>
-        </BugTable>
-    )
+    switch(actualIndex) {
+        case "Bugs":
+            return (
+                <BugTable>
+                    <thead>            
+                        <tr>
+                            <th>Image</th>
+                            <th>Name<br/>Price</th>
+                            <th>Time<br/>Location</th>
+                            <th>Season<br/>(Hemi.)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <BugMobileTable />
+                    </tbody>
+                </BugTable>
+            )
+        case "Fish":
+            return (
+                <BugTable>
+                    <thead>            
+                        <tr>
+                            <th>Image</th>
+                            <th>Name<br/>Price</th>
+                            <th>Time<br/>Location<br/>Size</th>
+                            <th>Season<br/>(Hemi.)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <FishMobileTable />
+                    </tbody>
+                </BugTable>
+            )   
+        case "Home":
+            return (
+                <HomeTable>
+                    <p>This is gatto from gattoalaparato</p>
+                    <p>thanx <a href={"www.twitter.com/gattoalaparato"}>@gattoalaparato</a></p>
+                </HomeTable>
+            )         
+        default: 
+                return <div>Error. Table not rendered.</div>
+    }
 }
-/*
-<th>Image</th>
-<th>Name</th>
-<th>Price</th>
-<th>Time</th>
-<th>Season</th>
-<th>Location</th> 
-*/
+
+const HomeTable = styled.div`
+    background-color: #A0D0E7;
+    width: 100%;
+    height: 99vh;
+    padding-top: 10%;
+    padding-left: 5%;
+`
 const BugTable = styled.table`
-    background-color: skyblue;
+    background-color: #F5F2E3;
     text-align: left;
     border-collapse: collapse;
     padding: 5px;
     max-width: 800px;
-    tr {
-        bgcolor: white;
-    }
+
     td {
         text-align: left;
+    }
+    tr {
+        background-color: #F5F2E3;
+        border-bottom: 16px solid #A0D0E7;
+        border-top: 16px solid #A0D0E7;
+        border-radius: 50%;
+    }
+    tr:nth-child(odd) {
     }
     td, th {
         padding: 0px 10px 10px 0px;
     }
     th {
-        padding-top: 10px;
+        padding-top: 15px;
     }
     td:first-child, th:first-child {
         padding-left: 10px;
@@ -109,11 +152,12 @@ const BugTable = styled.table`
         td, th {
             padding: 0;
             padding-bottom: 18px;
+            padding-top: 18px;
             text-align: center;
             vertical-align: top;
         }
         th{
-            padding-top: 40px;
+         background-color: #A0D0E7;
         }
         td:first-child, th:first-child {
             padding-left: 0px;
