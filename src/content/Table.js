529a8a6c2f407f1c1b4f728024ff8e6902e09a85
debello <42223093+debello@users.noreply.github.com>
2020-04-25 23:45:15 +0200

    Move back TableButtons to Table because Search

--- src/content/Table.js
+++ src/content/Table.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components'
 import bugJSON from '../data/bug-EN.json'
 import fishJSON from '../data/fish-EN.json'
+
+const imageURL = {
+    Price: "/img/icons/price1.jpg", 
+    ABC: "/img/icons/abc.png", 
+    Search: "/img/icons/search1.jpg", 
+    Reset: "/img/icons/reset1.png", 
+    Return: "/img/icons/return1.png"
+}
+  
+const TableButtons = ({actualIndex}) => {
+    const [sortBy, setSortBy] = useState("");
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const handleChange = event => {
+      setSearchTerm(event.target.value);
+    };
+
+    // Search Image
+    // <SearchImage src={imageURL.Search}  alt="Search" />
+    return (    
+        <>
+            <Ellipsis>
+                <SearchInput type="text" placeholder="Search" value={searchTerm} onChange={handleChange}>
+
+                </SearchInput>
+                <ABCButton onClick={() => setSortBy("ABC")}><ABCImage src={imageURL.ABC}  alt="ABC" /></ABCButton>
+                <PriceButton onClick={() => setSortBy("Price")}><PriceImage src={imageURL.Price}  alt="price" /></PriceButton>
+                <ResetButton onClick={() => setSortBy("Reset")}><ResetImage src={imageURL.Reset}  alt="Reset" /></ResetButton>
+            </Ellipsis>
+            <Table actualIndex={actualIndex} sortBy={sortBy} />
+        </>
+    )
+} 
+
+const Ellipsis = styled.div`
+  width: 100%;
+  height: 90px;
+  border-top-left-radius: 100%;
+  border-top-right-radius: 100%;
+  background-color: #A0D0E7;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
+const SearchInput = styled.input`
+  width: 100px;
+  height: 40px;
+  margin-top: 45px;
+  border-radius: 30px;
+  img {
+    float: right;
+  }
+`
+const ABCButton = styled.button`
+  width: 40px;
+  height: 40px;
+  margin-top: 45px;
+  border-radius: 10px;
+`
+const PriceButton = styled.button`
+  width: 40px;
+  height: 40px;
+  margin-top: 45px;
+  border-radius: 10px;
+`
+const ResetButton = styled.button`
+  width: 30px;
+  height: 30px;
+  margin-top: 45px;
+  border-radius: 50%;
+`
+const PriceImage = styled.img`
+  width: 100%;
+`
+const ABCImage = styled.img`
+  width: 100%;
+`
+const SearchImage = styled.img`
+  width: 30px;
+`
+const ResetImage = styled.img`
+  width: 100%;
+`
 // const BugDesktopTable = () => {
 //    const row = bugJSON.map(value =>
 //         <tr key={value.Number}>
@@ -188,4 +270,4 @@ const TableContainer = styled.table`
         };
     }
 `
-export { Table }
+export { TableButtons }
--- src/content/TableButtons.js
+++ src/content/TableButtons.js
@@ -1,78 +1,15 @@
+// Fichero Inhabilitado. Borrar de no encontrarle uso.
 import React, {useState} from 'react';
 import styled from 'styled-components'
 import { Table } from '../content/Table'
-
-const imageURL = {
-    Price: "/img/icons/price1.jpg", 
-    ABC: "/img/icons/abc.png", 
-    Search: "/img/icons/search1.jpg", 
-    Reset: "/img/icons/reset1.png", 
-    Return: "/img/icons/return1.png"
-}
   
 const TableButtons = ({actualIndex}) => {
-    const [sortBy, setSortBy] = useState("");
-
+    // Search Image
+    // <SearchImage src={imageURL.Search}  alt="Search" />
     return (    
         <>
-            <Ellipsis>
-                <SearchButton onClick={() => setSortBy("Search")}><SearchImage src={imageURL.Search}  alt="Search" /></SearchButton>
-                <ABCButton onClick={() => setSortBy("ABC")}><ABCImage src={imageURL.ABC}  alt="ABC" /></ABCButton>
-                <PriceButton onClick={() => setSortBy("Price")}><PriceImage src={imageURL.Price}  alt="price" /></PriceButton>
-                <ResetButton onClick={() => setSortBy("Reset")}><ResetImage src={imageURL.Reset}  alt="Reset" /></ResetButton>
-            </Ellipsis>
-            <Table actualIndex={actualIndex} sortBy={sortBy} />
         </>
     )
 } 
 
-const Ellipsis = styled.div`
-  width: 100%;
-  height: 90px;
-  border-top-left-radius: 100%;
-  border-top-right-radius: 100%;
-  background-color: #A0D0E7;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-`
-const SearchButton = styled.button`
-  width: 100px;
-  height: 40px;
-  margin-top: 45px;
-  border-radius: 30px;
-  img {
-    float: right;
-  }
-`
-const ABCButton = styled.button`
-  width: 40px;
-  height: 40px;
-  margin-top: 45px;
-  border-radius: 10px;
-`
-const PriceButton = styled.button`
-  width: 40px;
-  height: 40px;
-  margin-top: 45px;
-  border-radius: 10px;
-`
-const ResetButton = styled.button`
-  width: 30px;
-  height: 30px;
-  margin-top: 45px;
-  border-radius: 50%;
-`
-const PriceImage = styled.img`
-  width: 100%;
-`
-const ABCImage = styled.img`
-  width: 100%;
-`
-const SearchImage = styled.img`
-  width: 30px;
-`
-const ResetImage = styled.img`
-  width: 100%;
-`
 export { TableButtons } 
