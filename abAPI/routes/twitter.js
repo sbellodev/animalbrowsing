1e6c7a31d82d01dc0867d94953daaf27b0ab4464
debello <42223093+debello@users.noreply.github.com>
2020-05-08 18:25:39 +0200

    Add useContext for fetching Server fake API

--- abAPI/routes/twitter.js
+++ abAPI/routes/twitter.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 //
 router.get('/', function(req, res, next){
-    res.send('TW API is working. 2.0')
+    res.send('TW API is working. 3.0')
     //res.send('TW 2');
 });
 
--- src/content/Turnip.js
+++ src/content/Turnip.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components'
-const URL = 'http://localhost:9000/twitter'
-fetch(URL)
-    .then(res => res.text())
-    .then(json => console.log(json))
-
 
 const Turnip = () => {
+    const [Response, setResponse] = useState('Default Response')
+    
+    // useContext doc - https://upmostly.com/tutorials/how-to-use-the-usecontext-hook-in-react
+    const TestContext = React.createContext(Response)
+    const DisplayContext = useContext(TestContext)
+    
+    const URL = 'http://localhost:9000/twitter'
+    fetch(URL)
+        .then(res => res.text())
+        .then(json => setResponse(json))
+
     return (
         <TurnipContainer>
             <p>Hello, this is Turnip</p>
+            {DisplayContext}
             <br/>
             {/* <a className="twitter-timeline" style={{textAlign: "center"}} data-width="90%" data-theme="dark" href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a>  */}
         </TurnipContainer>
