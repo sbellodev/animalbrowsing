bd23413463a582b4b1a56fc46fbad1507b93f795
debello <42223093+debello@users.noreply.github.com>
2020-05-08 19:59:15 +0200

    Show tweets on the Client side

--- abAPI/routes/twitter.js
+++ abAPI/routes/twitter.js
@@ -2,11 +2,6 @@ var express = require("express")
 var router = express.Router()
 var Twit = require('twit')
 // const notifier = require('node-notifier');
-// const open = require('open');
-// const franc = require('franc')
-
-
-
 const consumer_key =       "1UC2bP9BteFaqAh7QTBSxIitz"
 const consumer_secret =    "cTZ2MH8z7keQaWtjClvSuevDlWGWSMl6QG0xd5iv8Tn8xjWuV1"
 const access_token =       "1258491948396150789-7Y64qbxKDgs0quQ5EOYaGiVGEcfPLV" 
@@ -19,42 +14,16 @@ var T = new Twit({
   access_token_secret:  access_token_secret,
 });
 
-const tweets = ""
-
-T.get('search/tweets', { q: '#AnimalCrossing turnip ', count: 5 }, function(err, data, response) {
-  const tweets = data.statuses
-  // .map(tweet => `LANG: ${franc(tweet.text)} : ${tweet.text}`) //CHECK LANGUAGE
-  .map(tweet => tweet.text)
-//   .filter(tweet => tweet.toLowerCase().includes('elon'));
-  console.log(tweets);
+T.get('search/tweets', { q: '#AnimalCrossing turnip ', count: 5 }, (err, data, response) => {
+    const tweets = data.statuses
+    // .map(tweet => `LANG: ${franc(tweet.text)} : ${tweet.text}`) //CHECK LANGUAGE
+    .map(tweet => tweet.text)
+    //   .filter(tweet => tweet.toLowerCase().includes('elon'));
+    router.get('/', (req, res, next) => {
+        res.send(tweets)
+        //res.send('TW 2');
+    });
 })
-//(async () => {
-
-    //1. GET RECENT TWEETS
-
-    
-    //SHOW NOTIFICATION FOR EACH RECEIVED TWEET
-    // stream.on('tweet', function (tweet) {
-    //   console.log(tweet.text);
-    //   let url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-
-    //   notifier.notify({
-    //     title: tweet.user.name,
-    //     message: tweet.text
-    //   });
-
-    //   notifier.on('click', async function(notifierObject, options, event) {
-    //     console.log('clicked');
-    //     await open(url);
-    //   });
-    // })
-//})();
-
-
-
-router.get('/', function(req, res, next){
-    res.send("test")
-    //res.send('TW 2');
-});
+console.log(T)
 
 module.exports = router;
