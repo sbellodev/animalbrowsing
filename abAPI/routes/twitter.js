b10c52aa267124118b1f05f0533d77b4fae0e3c2
debello <42223093+debello@users.noreply.github.com>
2020-05-09 18:01:26 +0200

    Add Server Call to twitter API

--- abAPI/routes/twitter.js
+++ abAPI/routes/twitter.js
@@ -6,7 +6,7 @@ const consumer_key =       "1UC2bP9BteFaqAh7QTBSxIitz"
 const consumer_secret =    "cTZ2MH8z7keQaWtjClvSuevDlWGWSMl6QG0xd5iv8Tn8xjWuV1"
 const access_token =       "1258491948396150789-7Y64qbxKDgs0quQ5EOYaGiVGEcfPLV" 
 const access_token_secret ="9x3amlTkGFef8CPlxhs3RoQ3n5wYIeijTyxN1F7eDcalq"
-
+// documentation - https://github.com/tombaranowicz/TwitterMonitoringJavaScript
 var T = new Twit({
   consumer_key:         consumer_key,
   consumer_secret:      consumer_secret,
@@ -14,16 +14,27 @@ var T = new Twit({
   access_token_secret:  access_token_secret,
 });
 
-T.get('search/tweets', { q: '#AnimalCrossing turnip ', count: 5 }, (err, data, response) => {
-    const tweets = data.statuses
-    // .map(tweet => `LANG: ${franc(tweet.text)} : ${tweet.text}`) //CHECK LANGUAGE
-    .map(tweet => tweet.text)
-    //   .filter(tweet => tweet.toLowerCase().includes('elon'));
-    router.get('/', (req, res, next) => {
-        res.send(tweets)
-        //res.send('TW 2');
-    });
-})
-console.log(T)
+function callAPI() {
 
+}
+//console.log(T)
+//testdata()
+
+  router.get('/', (req, res, next) => {
+    T.get('search/tweets', { q: '#AnimalCrossing turnip', count: 5 },
+    // const tweets = data.statuses
+    // .map(tweet => tweet.text)
+    // .map(tweet => `LANG: ${franc(tweet.text)} : ${tweet.text}`) //CHECK LANGUAGE
+    //   .filter(tweet => tweet.toLowerCase().includes('elon'));
+//},
+ function testdata(err, data, response) {
+  const tweets = data.statuses
+   .map(tweet => tweet.text)
+    console.log(tweets)
+    res.send(tweets)
+  })
+
+    //console.log(tweets)
+    //res.send('TW 2');
+  })
 module.exports = router;
