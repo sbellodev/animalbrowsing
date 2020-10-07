218ae0365e581d94b95c4f7a3bf694515a45b8a2
Santi Bello <santidebello@gmail.com>
2020-10-07 20:36:22 +0200

    split renderAPI logic function

--- src/content/Turnip.js
+++ src/content/Turnip.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
+import { contentAPI } from '../logic/apiTurnip.js'
 
 const Turnip = () => {
     const [APIResponse, setAPIResponse] = useState("")
+    let message_wait = <p className="tweet_individual" style={{height: "99vh"}}>Cargando tweets... si tarda mucho, por favor contacta con el webmaster.</p>
     
     const callServerAPI = () => {
         const URL = 'https://animalbrowsing.com/api'
@@ -20,51 +22,11 @@ const Turnip = () => {
         return () => clearInterval(intervalId)
     },[]) 
     
-    let message_wait
-    message_wait = <p className="tweet_individual" style={{height: "99vh"}}>Cargando tweets... si tarda mucho, por favor contacta con el webmaster.</p>
-    // if(localStorage.getItem("language") === "es"){
-    // }
-    // else {
-    //     message_wait = <p className="tweet_individual" style={{height: "99vh"}}>Loading tweets... please wait... if it lasts too much please contact the webmaster</p>
-    // }
-    
-    const renderTwitterAPIContent = (res) =>  {
-        let shortUrl = /https:\/\/t\.co\/+.{10}/g
-        let showResponse = res.map(tweet => {
-            if(tweet.text){
-                tweet.text = tweet.text.replace("'\n' +",  "")
-                if(tweet.entities.media){ // has image
-                    tweet.text = tweet.text.replace(tweet.entities.media[0].url, "<img src='" +tweet.entities.media[0].media_url+ "' />")
-                }
-                // urls[0] needed because API structure
-                if(tweet.entities.urls && tweet.entities.urls[0]){ // has other urls
-                    let num_of_urls = tweet.entities.urls.length 
-                    let last_url = num_of_urls - 1
-                    if(tweet.entities.urls[last_url].expanded_url.includes("/i/web/status")){ // remove status url
-                        tweet.text = tweet.text.replace(tweet.entities.urls[last_url].url, "")
-                    }
-                    tweet.entities.urls.forEach((url)=>{ // clickable links
-                        tweet.text = tweet.text.replace(shortUrl, "<br/><a href='" +url.expanded_url+ "' target='_blank' rel='noopener noreferrer' >" +url.expanded_url.slice(0, 23)+ "...</a><br/>")
-                    })
-                }
-            }
-
-            return   '<div class="tweet_individual">'
-                     + '<a href="https://twitter.com/' +tweet.screen_name+ '" target="_blank" rel="noopener noreferrer" style="display:flex;align-items:center;"><div style="padding-bottom:0"><img style="width:35px;border-radius:20px;display:inline;margin: 0 auto;" src="' +tweet.profile_image+ '"/></div><span style="margin:10px;">@' +tweet.screen_name+ '</span></a>'
-                     + '<p>' +tweet.text+ '</p>'
-                     + '<p><a href="https://twitter.com/i/web/status/' +tweet.id+ '" target="_blank" rel="noopener noreferrer">Ver más</a></p>'
-                     + '</div>'
-            }
-        ).join('')
-
-        return '<div class="blob_container"><div class="blob red"></div></div>' + showResponse                
-    }
-
     return (
         <>
         <TurnipContainer>
             {!APIResponse && <TwitterContent>{message_wait}</TwitterContent>}
-            {APIResponse && <TwitterContent dangerouslySetInnerHTML={{__html: renderTwitterAPIContent(APIResponse)}} />}
+            {APIResponse && <TwitterContent dangerouslySetInnerHTML={{__html: contentAPI(APIResponse)}} />}
         </TurnipContainer>
         </>
     )
---
+++ src/logic/apiTurnip.js
@@ -0,0 +1,33 @@
+const contentAPI = (res) =>  {
+    let shortUrl = /https:\/\/t\.co\/+.{10}/g
+    let showResponse = res.map(tweet => {
+        if(tweet.text){
+            tweet.text = tweet.text.replace("'\n' +",  "")
+            if(tweet.entities.media){ // has image
+                tweet.text = tweet.text.replace(tweet.entities.media[0].url, "<img src='" +tweet.entities.media[0].media_url+ "' />")
+            }
+            // urls[0] needed because API structure
+            if(tweet.entities.urls && tweet.entities.urls[0]){ // has other urls
+                let num_of_urls = tweet.entities.urls.length 
+                let last_url = num_of_urls - 1
+                if(tweet.entities.urls[last_url].expanded_url.includes("/i/web/status")){ // remove status url
+                    tweet.text = tweet.text.replace(tweet.entities.urls[last_url].url, "")
+                }
+                tweet.entities.urls.forEach((url)=>{ // clickable links
+                    tweet.text = tweet.text.replace(shortUrl, "<br/><a href='" +url.expanded_url+ "' target='_blank' rel='noopener noreferrer' >" +url.expanded_url.slice(0, 23)+ "...</a><br/>")
+                })
+            }
+        }
+
+        return   '<div class="tweet_individual">'
+                    + '<a href="https://twitter.com/' +tweet.screen_name+ '" target="_blank" rel="noopener noreferrer" style="display:flex;align-items:center;"><div style="padding-bottom:0"><img style="width:35px;border-radius:20px;display:inline;margin: 0 auto;" src="' +tweet.profile_image+ '"/></div><span style="margin:10px;">@' +tweet.screen_name+ '</span></a>'
+                    + '<p>' +tweet.text+ '</p>'
+                    + '<p><a href="https://twitter.com/i/web/status/' +tweet.id+ '" target="_blank" rel="noopener noreferrer">Ver más</a></p>'
+                    + '</div>'
+        }
+    ).join('')
+
+    return '<div class="blob_container"><div class="blob red"></div></div>' + showResponse                
+}
+
+export { contentAPI }
