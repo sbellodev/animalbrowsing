096858e673b4c0662d61a3a8160d47d826cd3f35
Santi Bello <santidebello@gmail.com>
2021-05-21 14:59:14 +0200

    Change titles/descriptions SEO

--- src/content/Helmet.js
+++ src/content/Helmet.js
@@ -6,20 +6,20 @@ const RHelmet = () => {
     if(!pathname) pathname = "home"
     
     let helmet = {
-        "home_title" : "Animal Browsing, tu kit para sobrevivir en Animal Crossing.",
-        "home_desc" : "Encuentra aquí nabos a buen precio, PIN DODOs y todo sobre bichos, peces, criaturas del mar, fósiles y mucho más en Animal Crossing: New Horizons.",
-        "turnips_title" : "Encuentra nabos a buen precio y PIN DODO | Animal Crossing.",
-        "turnips_desc" : "Encuentra gente para vender tus nabos con PIN DODOS ahora mismo.",
-        "bugs_title" : "Lista Completa de Bichos | Animal Crossing: New Horizons.", 
-        "bugs_desc" : "Todo sobre Bichos de Animal Crossing: New Horizons. Mariposa colias, bianor, Cigarra gigante, escarabajo ciervo sierra...", 
-        "fish_title" : "Lista Completa de Peces | Animal Crossing: New Horizons.", 
-        "fish_desc" : "Todos los Peces de Animal Crossing. Amarguillo, Koi, Siluro, Salmón, Arowana, Pirarucú, Pez luna...", 
-        "sea-creatures_title" : "Lista Completa de Criaturas Marinas | Animal Crossing: New Horizons.", 
-        "sea-creatures_desc" : "Descubre todo sobre Criaturas Marinas y sus secretos. Alga wakame, Estrella de mar, Abulón, Buccino...",     
-        "fossil_title" : "Lista Completa de Fósiles | Animal Crossing: New Horizons.", 
-        "fossil_desc" : "Encuentra aquí todos los fósiles. Acantostega, Anquilosaurio, Pteranodonte, Tiranosaurio, Megaloceros, Iguanodonte...",
+        "home_title" : "Animal Browsing, tu kit para sobrevivir en Animal Crossing",
+        "home_desc" : "Encuentra aquí nabos a buen precio, PIN DODOs y todo sobre bichos, peces, criaturas del mar, fósiles y mucho más en Animal Crossing: New Horizons",
+        "turnips_title" : "Nabos a buen precio con PIN DODO | Animal Crossing",
+        "turnips_desc" : "Encuentra gente para vender tus nabos con PIN DODOS ahora mismo",
+        "bugs_title" : "Todos los Bichos | Animal Crossing: New Horizons", 
+        "bugs_desc" : "Lista completa de Bichos de Animal Crossing: New Horizons Mariposa colias, bianor, Cigarra gigante, escarabajo ciervo sierra ...", 
+        "fish_title" : "Todos los Peces | Animal Crossing: New Horizons", 
+        "fish_desc" : "Encuentra todo sobre Peces de Animal Crossing | Amarguillo, Koi, Siluro, Salmón, Arowana, Pirarucú, Pez luna ...", 
+        "sea-creatures_title" : "Todas las Criaturas Marinas del mar | Animal Crossing: New Horizons", 
+        "sea-creatures_desc" : "Descubre todo sobre Criaturas Marinas y sus secretos. Alga wakame, Estrella de mar, Abulón, Buccino ...",     
+        "fossil_title" : " Todos los Fósiles | Animal Crossing: New Horizons", 
+        "fossil_desc" : "Encuentra aquí todos los fósiles. Acantostega, Anquilosaurio, Pteranodonte, Tiranosaurio, Megaloceros, Iguanodonte ...",
         "shop_title": "Tienda Online de Animal Crossing: New Horizons",
-        "shop_desc": "Desde peluches hasta videojuegos, encuentra lo que buscas y mucho más en nuestra tienda online." 
+        "shop_desc": "Desde peluches hasta videojuegos, encuentra lo que buscas y mucho más en nuestra tienda online" 
     }
 
     return (
